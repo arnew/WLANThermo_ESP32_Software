2f80d6fb45246aca831db1e5e6ec6ff2887679e4
Arne Wichmann <335090+arnew@users.noreply.github.com>
2024-03-17 19:24:16 +0100

    Add Draft Implementation of Pitmaster Screen.

--- src/display/tft/lvPitmaster.h
+++ src/display/tft/lvPitmaster.h
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
     Copyright (C) 2020  Martin Koerner
 
     This program is free software: you can redistribute it and/or modify
@@ -13,40 +13,23 @@
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-    
+
     HISTORY: Please refer Github History
-    
+
 ****************************************************/
 #pragma once
 
 #include "Arduino.h"
 #include <lvgl.h>
 
-typedef void (*lvMenu_Create_FuncPtr)(void *userData);
-typedef void (*lvMenu_Update_FuncPtr)(boolean forceUpdate);
-typedef void (*lvMenu_Delete_FuncPtr)(void);
-
-typedef enum class lvScreen
+typedef struct lvPitmaster
 {
-  None = 0,
-  Menu,
-  Home,
-  Wifi,
-  Display,
-  Temperature,
-  Update,
-  Pitmaster,
-  Max
-} lvScreenType;
 
-typedef struct
-{
-  lvScreenType screen;
-  lvMenu_Create_FuncPtr createFunc;
-  lvMenu_Update_FuncPtr updateFunc;
-  lvMenu_Delete_FuncPtr deleteFunc;
+  lv_obj_t *screen;
+  lv_obj_t *spinbox;
 
-} lvScreenFuncType;
+} lvPitmasterType;
 
-void lvScreen_Open(lvScreenType screen, void *userData = NULL);
-void lvScreen_Update(void);
+void lvPitmaster_Create(void *userData);
+void lvPitmaster_Update(boolean forceUpdate);
+void lvPitmaster_Delete(void);
